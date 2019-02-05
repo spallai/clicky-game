b135863772fb6383bd0e4de0c6bade57fa825887
SPallai <sangeeta.sumair@gmail.com>
2019-02-04 19:08:54 -0800

    Updates

---
+++ precache-manifest.3953099808d982c454c81b50b53e2537.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "43973a61397a9aaebcc649020af4a8d7",
+    "url": "/clicky-game/static/media/batman.43973a61.jpg"
+  },
+  {
+    "revision": "33e73256a4603d9598ee",
+    "url": "/clicky-game/static/css/main.0f731174.chunk.css"
+  },
+  {
+    "revision": "5eba191d09662fca6549",
+    "url": "/clicky-game/static/js/1.5eba191d.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "992d78ed7e355c284db5b112126a94dc",
+    "url": "/clicky-game/static/media/Jessica-Rabbit.992d78ed.jpg"
+  },
+  {
+    "revision": "33e73256a4603d9598ee",
+    "url": "/clicky-game/static/js/main.33e73256.chunk.js"
+  },
+  {
+    "revision": "837c3511804bda0623e0af359b58aaa1",
+    "url": "/clicky-game/static/media/betty.837c3511.png"
+  },
+  {
+    "revision": "c1bb59d4966589ea866209cfb95b35b5",
+    "url": "/clicky-game/static/media/link.c1bb59d4.jpg"
+  },
+  {
+    "revision": "1d21724cfd1394421d666ee7669d5882",
+    "url": "/clicky-game/static/media/mr-peanutbutter-435.1d21724c.jpg"
+  },
+  {
+    "revision": "ec1591d4fd13279bf0ba67b7df3b67c3",
+    "url": "/clicky-game/static/media/pepe.ec1591d4.jpg"
+  },
+  {
+    "revision": "daa391a979e4043a843c07cf631feaca",
+    "url": "/clicky-game/index.html"
+  }
+];
